60b3bc9b88cd47b0ce4aa84ab6898d60a490263d
Akhilesh Boodhun <akhilesh.boodhun@umail.uom.ac.mu>
2021-02-25 23:55:28 +0400

    inter 2

--- Intersection.java
+++ Intersection.java
@@ -3,13 +3,24 @@ import java.util.*;
 public class Intersection {
     Integer intersection_id;
     CarQueues carQueues;
+    LinkedList<Integer> timers;
 
-    Intersection(int id, CarQueues cq){
+    Intersection(Integer id, CarQueues cq){
         this.intersection_id = id;
         this.carQueues = cq;
+        this.timers = new LinkedList<>();
     }
+
     Integer getId(){
         return this.intersection_id;
+    }
+
+    void setTimerByIndex(Integer index, Integer value){
+        this.timers.set(index, value);
+    }
+
+    void addTimer(Integer value){
+        this.timers.add(value);
     }
 
     CarQueues getQueues(){
