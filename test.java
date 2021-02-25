50cf557f22251257b5813ff70ad44e35c6aa83b9
reeshal <rreeshal@gmail.com>
2021-02-25 20:33:37 +0400

    a

--- test.java
+++ test.java
@@ -1,5 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class test{
     public static void main(String[] args){
         System.out.println("hello world");
+        try{
+            Scanner read= new Scanner(new File("test.txt"));
+			while(read.hasNextLine())
+				System.out.println(read.nextLine());
+			read.close();
+        }
+        catch(Exception e) {
+			System.out.println(e);
+		}	
     }
 }
